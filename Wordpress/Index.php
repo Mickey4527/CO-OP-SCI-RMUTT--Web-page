9ade25e903e7e99845e9430a7039aaa7a5a054b7
Saran458 <saran.za097@gmail.com>
2023-04-09 18:45:52 +0700

    fix home

--- Wordpress/Index.php
+++ Wordpress/Index.php
@@ -1,13 +1,34 @@
 ﻿<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query. 
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Graphene
+ * @since Graphene 1.0
+ */
+get_header(); ?>
 
-get_header(); 
-?>
-<?php
+<main id="main-content">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+            </header>
 
-$page = isset($_GET['page'] ) ? $_GET['page'] : 'home' ;
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
 
-	include($page.'.php');
-		
-?>
-	<?php
-		get_footer();
+            <footer class="entry-footer">
+                <?php the_tags( '<ul><li>', '</li><li>', '</li></ul>' ); ?>
+            </footer>
+        </article>
+    <?php endwhile; endif; ?>
+</main>
+
+<?php get_footer(); ?>
---
+++ Wordpress/functions.php
@@ -0,0 +1,9 @@
+<?php
+function custom_theme_styles() {
+  wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/bootstrap/css/bootstrap.css' );
+  wp_enqueue_style( 'custom-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'custom_theme_styles' );
+
+
+?>
